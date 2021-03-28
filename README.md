d176a1ca96eb830664aa964403520f340565c5f6
Nicholas Ang <Con.nang@cityofperth.wa.gov.au>
2021-03-28 14:40:27 +0800

    Initial check-in

---
+++ README.md
@@ -0,0 +1,21 @@
+# Update Library Document Set Welcome Page View using Playwright and Azure Function
+
+1. This is using [Playright](https://playwright.dev) which is pretty new and maintained by Microsoft.
+2. This uses a Linux Docker Azure Function so you will have to use Visual Studio or Visual Studio code to deploy.
+3. This was made to faciliate https://github.com/pnp/pnpframework/issues/105 till there is a proper solution from the PnP Framework.
+
+## Things to note
+1. If you are using VS Code ensure your ```.vscode/settings.json``` has 
+```"azureFunctions.scmDoBuildDuringDeployment": true```
+2. Ensure Azure Function has ```"PLAYWRIGHT_BROWSERS_PATH" :0``` in its function app settings.
+3. This require a SharePoint Admin account providing username and password to Playwright to get access to the required page as SharePoint isn't an anonymous access system. I've followed Elio's method for the username and password. File is stored in the Auth folder
+<pre>To make it a bit “safer” I added a dependency called cpass in order to encode and decode the passwords. In the project, you find an encoder.js file which you can use to encode your username and password. Add your username and password in it, and run node encoder.js, after this you can remove the file.Username and Password is encoded using CPASS.</pre>
+
+## References
+1. https://anthonychu.ca/post/azure-functions-headless-chromium-puppeteer-playwright/
+2. https://dotnetthoughts.net/running-playwright-on-azure-functions/
+3. https://www.eliostruyf.com/testing-the-ui-of-your-spfx-solution-with-puppeteer-and-jest/
+
+
+## TODO
+1. Use PNPJS to get the Content Type and List ID based on the title.
---
+++ Update-LibraryDocSetWelcomePageView/index.js
@@ -0,0 +1,68 @@
+// Global
+const { chromium } = require("playwright-chromium");
+const spauth = require("node-sp-auth");
+const Cpass = require('cpass').Cpass;
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger beginning Update-DocSetWelcomePageView request.');
+
+    // Note that this is for Azure Function so there is a request body to be sent along.
+    if (req.body) {
+        const groupAlias = req.body.GroupAlias;
+        const contentTypeID = req.body.ContentTypeID;
+        const listID = req.body.ListID;
+        const welcomePageViewName = req.body.WelcomePageViewName;
+    }
+
+    // Doc Set Setting page to go to     
+    const pageUrl = " https://" + process.env.tenantName + ".sharepoint.com/sites/" + groupAlias + "/_layouts/15/docsetsettings.aspx?ctype=" + contentTypeID + "&List=" + listID;
+
+    // Authentication setup
+    const cpass = new Cpass();
+    const authObject = await spauth.getAuth(pageUrl, {
+        username: cpass.decode(process.env.spAdminUsername),
+        password: cpass.decode(process.env.spAdminPassword)
+    });
+
+    // Do the magic
+    // Playwright Automation
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await setDocSetWelcomePageView(authObject, page, pageUrl, welcomePageViewName);
+    const responseBody = `Document library is now set. Visit <a href='${pageUrl}'>Document Set Settings Page</a> to verify`
+    await browser.close();
+    
+    // Return response
+    context.res = {
+        status: 200,
+        body: responseBody,
+        headers: {
+            "content-type": "text/html"
+        }
+    };
+}
+
+// Custom Helper Functions
+async function scrollOnElement(page, selector) {
+    await page.$eval(selector, (element) => {
+        element.scrollIntoView();
+    });
+}
+
+async function setDocSetWelcomePageView(authObject, page, pageUrl, welcomePageViewName) {
+
+    // Add the authentication headers
+    await page.setExtraHTTPHeaders(authObject.headers);
+    await page.goto(pageUrl);
+
+    // Scroll to where the Welcome Page View Dropdown is so that the automation tool can find it
+    await page.waitForSelector("#ctl00_PlaceHolderMain_idWelcomePageView_ctl01_DropDownListViews");
+    await scrollOnElement(page, "#ctl00_PlaceHolderMain_idWelcomePageView_ctl01_DropDownListViews");
+
+    // Update the Welcome Page View
+    await page.click("#ctl00_PlaceHolderMain_idWelcomePageView_ctl01_DropDownListViews");
+    await page.selectOption("[name='ctl00$PlaceHolderMain$idWelcomePageView$ctl01$DropDownListViews']", [{ 'label': welcomePageViewName }]);
+
+    // Submit and leave
+    await page.click("[type='submit']");
+}
